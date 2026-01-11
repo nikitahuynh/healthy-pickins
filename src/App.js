c1e6d8b9499120e7401ba5cb8f25259643719a3d
Emily Lao <emilylao@ucsb.edu>
2026-01-10 23:18:31 -0800

    fixed ecopic errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'; // Import the new CSS file
-import EcoLogo from './EcoPic4Edited.jpeg';
 import { createClient } from '@supabase/supabase-js';
 import './App.css';
 import EcoLogo from './EcoPic5.png';
